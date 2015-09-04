c9ee116a9398e4a7e1785ce61a973fe9328f2f7b
Chris Wallwork <chris@unrefgugees.org>
2015-09-04 09:19:16 -0400

    handled submit button, send to mailchimp, response handled. Animated submit button and confirmation message, currently in test mode

--- _assets/javascripts/app.js
+++ _assets/javascripts/app.js
@@ -2,16 +2,47 @@
 
 $(document).ready(function(){
   console.log("ready");
-  submitEventListener();
+  submitEventListenerTest();
 
 });
 
-function submitEventListener(){
-  var form = $("#signup-form");
+function submitEventListenerTest(){
+  $button = $(".submit-button");
 
-  form.submit(function(event){
+  $button.click(function(event){
     event.preventDefault();
-    var data = form.serialize();
-    console.log(data);
+    $('.submit-button').slideUp(1000, function(){
+      $('#conf-message').html("We sent you an email. Please confirm your address to receive your sticker").slideDown(1000);
+    });
+    
+  });
+}
+
+function submitEventListener(){
+  $form = $("#signup-form");
+
+  $form.submit(function(event){
+    event.preventDefault();
+    $.ajax({
+      type: 'POST',
+      url: '//nyc.us11.list-manage.com/subscribe/post-json?u=7aa897cfc40f7cfbb83ffadd4&amp;id=5c1d326f5e&c=?',
+      data: $form.serialize(),
+      timeout: 5000,
+      cache: false,
+      dataType: 'jsonp',
+      contentType: "application/json; charset=utf-8",
+      error: function(err) {console.log("something went wrong")},
+      success: function(data){
+        if (data.result != "success") {
+          console.log(data.msg);
+        }
+        else {
+          $('form').slideDown(800, function(){
+            $('#conf-message').html('We sent you an email, please confirm to receive your bumper sticker!').fadeIn(700);
+          });
+          
+        }
+      }
+    });
   });
 };
