02e52692b6566918af1a05980e2c86342dcb6350
Chris Wallwork <chris@unrefgugees.org>
2015-12-15 16:13:59 -0500

    added signup form js

--- _assets/javascripts/app.js
+++ _assets/javascripts/app.js
@@ -1,7 +1,118 @@
 'use-strict'
 
-$(document).ready(function(){
-  submitEventListener();
-
+var gw = new Groundwork({
+    'api_url': 'https://api.thegroundwork.com',
+    'oauth_client_id': 'pub-un-test.jesus-was-a-refugee-test-int-MUEAS9X5941jPRczITpf0anqMFqunzzh6YAgImx0EUT18aBcBPIIVdEsx7wK4kcSibY3hpFhpv9H0_GgOUp4SA'
 });
 
+var fields = { phone: null, city: null, state: null};
+
+$(document).ready(function(){
+  createEventListeners();
+});
+
+function sendData(data,form){
+  data.tags = (data.tags || {});
+  data.tags.send_email = 0;
+  gw.supporters.create(data)
+  .then(function(response){
+      console.log(response);
+      processResponse('success',form);
+  })
+  .catch(function(response){
+      debugger;
+      console.log(response);
+      processResponse('failure',form);
+  });
+};
+
+function createEventListeners(){
+  storyListener();
+  cardListener();
+  celebrateListener();
+}
+
+function storyListener(){
+  $('.story-form').submit(function(event){
+    event.preventDefault();
+    var data = collectData($('.story-form'));
+    data.source = "refugee-jesus story-signup";
+    sendData(data,'story');
+  });
+}
+
+function cardListener(){
+  $('.card-form').submit(function(event){
+    event.preventDefault();
+    var data = collectData($('.card-form'));
+    data.source = "refugee-jesus card-signup";
+    sendData(data,'card');
+  });
+}
+
+function celebrateListener(){
+  $('.celebrate-form').submit(function(event){
+    event.preventDefault();
+    var data = collectData($('.celebrate-form'));
+    data.source = "refugee-jesus celebrate-signup";
+    sendData(data,'celebrate');
+  });
+}
+
+function collectData(form){
+  var data = {};
+  data.tags = {};
+  var inputs = form.find(":input");
+  $(inputs).each(function(key,value){
+    if($(this).attr('name') === "name"){
+      var names = splitNames($(this).val());
+      $.each(names, function(key, value){
+        data[key] = value;
+      });
+    }
+    else if($(this).attr('name') === "email"){
+      data.email = $(this).val();
+    }
+    else {
+      $(this).val() != "" ? createProp($(this).attr('name') , $(this).val() , data) : null;
+    }
+  });
+  return data;
+}
+
+function createProp(attribute,value,data){
+  if(fields.hasOwnProperty(attribute)){
+    data[attribute] = value;
+  }
+  else {
+    data.tags[attribute] = value;
+  }
+}
+
+
+function splitNames(name){
+  var names = name.split(" ",2);
+  var data = {};
+  data.givenName = names[0];
+  names[1] ? data.familyName = names[1] : null ;
+  return data;
+}
+
+function processResponse(status,destination){
+  var form = $('.' + destination + '-form');
+  debugger; 
+  var msg = $('.' + destination + '-msg');
+
+  
+  if(status === 'success'){
+    msg.html('Thanks for signing up!');
+    msg.collapse('show');
+    form.find('input').prop('disabled',true);
+    form.find('button').prop('disabled',true);
+  }
+  else{
+    msg.html("something went wrong, please try again");
+    msg.collapse('show');
+    form[0].reset();
+  }
+}
